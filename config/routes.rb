45a0d582e2e0bfc323dbb02ea6cd09bd62798c3a
Barrett Helzel <barrett.p.helzel@gmail.com>
2020-05-13 10:12:39 -0700

    create custom question routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  namespace :api, defaults { format: :json } do
-    resource :user only: [:create]
+  namespace :api, defaults: { format: :json } do
+
+    resource :user, only: [:create]
     resource :session, only: [:create, :destroy, :show]
+
     resources :questions do
-      get 'test', on :test
-      get 'category', on: :category
+      collection do
+        get 'test'
+        get 'category'
+      end 
     end
+
+  end
+
+  root "static_pages#root"
 end
