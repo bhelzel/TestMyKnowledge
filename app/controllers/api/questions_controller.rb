cec4d2c06c57dc802bb178f902da74aa224fef11
Barrett Helzel <barrett.p.helzel@gmail.com>
2020-05-13 09:49:44 -0700

    write questions_controller

--- app/controllers/api/questions_controller.rb
+++ app/controllers/api/questions_controller.rb
@@ -1,3 +1,31 @@
 class Api::QuestionsController < ApplicationController
     
+    attr_reader :questions
+
+    def index
+        @questions = Question.all()
+        render "api/questions/index"
+    end
+
+    def knowledge_test
+        @questions = Question.find_by(params[:knowledge_test]))
+    end
+
+    def category
+        @questions = Question.find_by(params[:category])
+    end
+
+    private
+
+    params.requre(:questions).permit(
+        :question_text,
+        :answer,
+        :answers_array,
+        :category,
+        :difficulty,
+        :knowledge_test
+    )
+    
+
+
 end
